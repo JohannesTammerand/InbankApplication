02f4fabd3ac18f8951ceeb0a0a2e73cdca129114
GitMyDik <57413326+GitMyDik@users.noreply.github.com>
2025-03-26 15:57:33 +0200

    TICKET-102 | Turned constants class into a repository, added life expectancies for the baltics

--- intern-decision-engine-backend/src/main/java/ee/taltech/inbankbackend/config/DecisionEngineConstants.java
+++ intern-decision-engine-backend/src/main/java/ee/taltech/inbankbackend/config/DecisionEngineConstants.java
@@ -1,9 +1,14 @@
 package ee.taltech.inbankbackend.config;
 
+import jakarta.annotation.PostConstruct;
+import org.springframework.stereotype.Repository;
+
+import java.util.TreeMap;
+
 /**
  * Holds all necessary constants for the decision engine.
  */
-
+@Repository
 public class DecisionEngineConstants {
     public static final Integer MINIMUM_LOAN_AMOUNT = 2000;
     public static final Integer MAXIMUM_LOAN_AMOUNT = 10000;
@@ -12,4 +17,13 @@ public class DecisionEngineConstants {
     public static final Integer SEGMENT_1_CREDIT_MODIFIER = 100;
     public static final Integer SEGMENT_2_CREDIT_MODIFIER = 300;
     public static final Integer SEGMENT_3_CREDIT_MODIFIER = 1000;
+
+    public static final TreeMap<String, Integer> LIFE_EXPECTANCY_BY_COUNTRY = new TreeMap<>();
+
+    @PostConstruct
+    public void init(){
+        LIFE_EXPECTANCY_BY_COUNTRY.put("Estonia", 75);
+        LIFE_EXPECTANCY_BY_COUNTRY.put("Latvia", 80);
+        LIFE_EXPECTANCY_BY_COUNTRY.put("Lithuania", 70);
+    }
 }
